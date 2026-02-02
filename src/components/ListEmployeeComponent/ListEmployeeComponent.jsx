f6224c271ad3bfa7ebfd72d5ce2a37cc1dbbaccf
Chamudika07 <214651110+Chamudika07@users.noreply.github.com>
2026-02-02 10:28:26 +0530

    FEATCH DATA AND DESPLAY EMPLOY

--- src/components/ListEmployeeComponent/ListEmployeeComponent.jsx
+++ src/components/ListEmployeeComponent/ListEmployeeComponent.jsx
@@ -1,27 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { listEmployees } from '../services/EmployeeService';
 
 const ListEmployeeComponent = () => {
 
-  const dummyData = [
-    {
-      id: 1,
-      firstName: "Chamudika",
-      lastName: "Pramod",
-      email: "chamudikapramod23@gmail.com"
-    },
-    {
-      id: 2,
-      firstName: "Sanoshi",
-      lastName: "Niwarhthana",
-      email: "sanoshi@gmail.com"
-    },
-    {
-      id: 3, // ⚠️ id should be unique
-      firstName: "Inoka",
-      lastName: "Malanthi",
-      email: "inokka@gmail.com"
-    }
-  ];
+  // const dummyData = [
+  //   {
+  //     id: 1,
+  //     firstName: "Chamudika",
+  //     lastName: "Pramod",
+  //     email: "chamudikapramod23@gmail.com"
+  //   },
+  //   {
+  //     id: 2,
+  //     firstName: "Sanoshi",
+  //     lastName: "Niwarhthana",
+  //     email: "sanoshi@gmail.com"
+  //   },
+  //   {
+  //     id: 3, // ⚠️ id should be unique
+  //     firstName: "Inoka",
+  //     lastName: "Malanthi",
+  //     email: "inokka@gmail.com"
+  //   }
+  // ];
+
+  const [employees , setEmployees] = useState([]);
+
+useEffect(() => {
+  listEmployees()
+    .then((response) => {
+      setEmployees(response.data);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+}, []);
+
 
   return (
     <div className="container mt-4">
@@ -38,7 +52,7 @@ const ListEmployeeComponent = () => {
         </thead>
 
         <tbody>
-          {dummyData.map(employee => (
+          {employees.map(employee => (
             <tr key={employee.id}>
               <td>{employee.id}</td>
               <td>{employee.firstName}</td>
